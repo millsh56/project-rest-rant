14f9a5c5c281baf333784fa88dc53a89e2e2a05c
Hannah Mills <millsh56@gmail.com>
2023-08-20 10:46:17 -0400

    adding 404 page route

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<1>404 Page</h1>')
+})
+
 app.listen(process.env.PORT)
+
+
+
+
