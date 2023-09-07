e553825300b7b8c40e405decf5a31c56f172640b
Hannah Mills <millsh56@gmail.com>
2023-09-06 23:43:31 -0400

    adding css styling and starting index

--- views/home.jsx
+++ views/home.jsx
@@ -6,12 +6,24 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+          <h1>REST-Rant</h1>
+              <div className= 'h-50 d-inline-block'>
+      <img className= 'img-fluid' src="/images/home-img.jpg" alt="Fancy drink and food dish" />
+      </div>
+      <div>
+        <a href="https://unsplash.com/@sharonchen">Photo by Sharon Chen</a> on <a href="https://unsplash.com/">Unsplash</a>
+      </div>
               <a href="/places"/>
-  <button className="btn-primary">Places Page</button>
+  <button className="btn btn-primary">Places Page</button>
           </main>
       </Def>
     )
   }  
 
 module.exports = home
+
+
+
+
+    
+  
