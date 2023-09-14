e9cd8f837506c3ebb90944e739c249b389f815f6
Hannah Mills <millsh56@gmail.com>
2023-09-14 17:52:03 -0400

    added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -60,6 +60,20 @@ router.get('/', (req, res) => {
   }
 })
 
+// EDIT
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 
 module.exports = router
 
