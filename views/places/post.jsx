715410ff186a3b49a832ff6ed562cf307121a824
Hannah Mills <millsh56@gmail.com>
2023-09-14 14:22:13 -0400

    adding styled nav bar

--- views/places/post.jsx
+++ views/places/post.jsx
@@ -1,28 +1,26 @@
 const React = require('react')
 const Def = require('../default')
 
-function Post (place) {
-    return (
-      <Def>
-          <main>
-              <h1>{place.name} Page</h1>
-    <p>
-    Located in {place.city}, {place.state}
-  </p>
-  <img src={place.img} alt={place.name} />
+function Post(place) {
+  return (
+    <Def>
+      <main>
+        <h1>{place.name} Page</h1>
+        <p>
+          Located in {place.city}, {place.state}
+        </p>
+        <img src={place.img} alt={place.name} />
 
-<a href={`/places/${place.id}/edit`}><button>Edit</button></a>
-  
-<form action={`/places/${place.id}?_method=DELETE`} method="POST">
-  <input type='submit' value="DELETE"/>
-</form>
+        <a href={`/places/${place.id}/edit`}><button>Edit</button></a>
 
-  <li><a href="/places">Go home</a></li>
-          </main>
-      </Def>
-    )
-  }  
+        <form action={`/places/${place.id}?_method=DELETE`} method="POST">
+          <input type='submit' value="DELETE" />
+        </form>
+      </main>
+    </Def>
+  )
+}
 
-  
+
 
 module.exports = Post
